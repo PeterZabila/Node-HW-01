d2b1eb9e77fcc25a70c1ace52e35621c28684e2e
PZ <wikiwoo@PZ.local>
2022-11-28 01:44:07 +0200

    contacts.js almost ready

---
+++ contacts.js
@@ -0,0 +1,80 @@
+const fs = require("fs").promises;
+const path = require('path');
+
+const contactsPath = path.resolve('db/contacts.json');
+console.log(contactsPath);
+
+
+function listContacts() {
+    fs.readFile(contactsPath)
+    .then((data) => console.table(JSON.parse(data)))
+    .catch(err => console.log(err.messaage))
+  }
+  
+  function getContactById(contactId) {
+    fs.readFile(contactsPath)
+    .then((data) =>
+      JSON.parse(data).find(
+        (contact) => contact.id.toString() === contactId.toString()
+      )
+    )
+    .then((result) =>
+      result ? console.log(result) : console.log("Contact not found")
+    )
+    .catch((err) => console.log(err.message));
+  }
+  
+  function removeContact(contactId) {
+    fs.readFile(contactsPath)
+    .then((data) =>
+      JSON.parse(data).find(
+        (contact) => contact.id.toString() === contactId.toString()
+      )
+        ? JSON.parse(data).filter(
+            (contact) => contact.id.toString() !== contactId.toString()
+          )
+        : (result = null)
+    )
+    .then((result) =>
+      result !== null
+        ? fs
+            .writeFile(testPath, JSON.stringify(result))
+            .then(() => console.log(`Contact ${contactId} removed`))
+            .catch((err) => console.log(err.message))
+        : console.log("Contact not found")
+    )
+    .catch((err) => console.log(err.message));
+  }
+  
+  function addContact(name, email, phone) {
+    s.readFile(contactsPath)
+    .then((data) => {
+      const contactsList = JSON.parse(data);
+      if (
+        contactsList.find(
+          (contact) => contact.name.toLowerCase() === name.toLowerCase()
+        ))
+       {
+        console.log(`${name} is already in contacts`);
+        return;
+      }
+
+      contactsList.push({ name, email, phone });
+      fs.writeFile(contactsPath, JSON.stringify(contactsList)).then(
+        console.log(`${name} is added to contacts`)
+      );
+      return;
+    })
+
+    .catch((err) => console.log(err.message));
+  }
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+};
+
+getContactById("3");
+// listContacts();
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello");
+
+const logger = require('./contacts');
+
+logger.listContacts();
+logger.getContactById(3);
+logger.removeContact(2);
+logger.addContact("Allen Raymond",
+"nulla.ante@vestibul.co.uk", "(992) 914-3792")
