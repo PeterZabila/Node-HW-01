d3127ede76a3c8274a61f665b97e75b3bc94a457
ZABILA PETRO <petrozabila@gmail.com>
2022-12-01 13:11:00 +0200

    HW fixes after review

--- contacts.js
+++ contacts.js
@@ -1,92 +1,47 @@
 const fs = require("fs").promises;
 const path = require('path');
+const {nanoid} = require("nanoid")
 
 const contactsPath = path.resolve('db/contacts.json');
 
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  console.table(JSON.parse(data));
+}
 
-function listContacts() {
-    fs.readFile(contactsPath)
-    .then((data) => console.table(JSON.parse(data)))
-    .catch(err => console.log(err.messaage))
-  }
-  
-  function getContactById(contactId) {
-    fs.readFile(contactsPath)
-    .then((data) =>
-      JSON.parse(data).find(
-        (contact) => contact.id.toString() === contactId.toString()
-      )
-    )
-    .then((result) =>
-      result ? console.log(result) : console.log("Contact not found")
-    )
-    .catch((err) => console.log(err.message));
-  }
-
-
-const getAll = async() => {
-    const data = await fs.readFile(contactsPath);
-    return JSON.parse(data);
+const getContactById = async (contactId) => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  const result = contacts.find(contact => contact.id.toString() === contactId.toString())   
+    result ? console.log(result) : console.log("Contact not found")
 }
 
 const removeContact = async (contactId) => {
-  const contacts = await getAll();
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
   const index = contacts.findIndex(item => item.id.toString() === contactId.toString());
   if(index === -1) {
       return null;
   }
   const [result] = contacts.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  console.log(result);
-  return result;
+  console.log(`${result.name} sucessfully removed from the contact list`);
 }
-
-// function removeContact(contactId) {
-//   fs.readFile(contactsPath)
-//     .then((data) =>
-//       JSON.parse(data).find(
-//         (contact) => contact.id.toString() === contactId.toString()
-//       )
-//         ? JSON.parse(data).filter(
-//             (contact) => contact.id.toString() !== contactId.toString()
-//           )
-//         : (result = null)
-//     )
-//     .then((result) =>
-//       result !== null
-//         ? fs
-//             .writeFile(contactsPath, JSON.stringify(result))
-//             .then(() => console.log(`Contact with ID ${contactId} removed`))
-//             .catch((err) => console.log(err.message))
-//         : console.log("Contact not found")
-//     )
-//     .catch((err) => console.log(err.message));
-// }
-
   
-  
-  function addContact(name, email, phone) {
-    fs.readFile(contactsPath)
-    .then((data) => {
-      const contactsList = JSON.parse(data);
-      if (
-        contactsList.find(
-          (contact) => contact.name.toLowerCase() === name.toLowerCase()
-        ))
-       {
-        console.log(`${name} is already in contacts`);
-        return;
-      }
+const addContact = async (name, email, phone) => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  const contactId = nanoid();
 
-      contactsList.push({ name, email, phone });
-      fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2)).then(
-        console.log(`${name} is added to contacts`)
-      );
+  if (contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+      console.log(`${name} is already in the contact list`);
       return;
-    })
-
-    .catch((err) => console.log(err.message));
   }
+  contacts.push({name, email, phone, id: contactId});
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  console.log(`${name} is added to contacts`)
+  return;
+}
 
 module.exports = {
     listContacts,
